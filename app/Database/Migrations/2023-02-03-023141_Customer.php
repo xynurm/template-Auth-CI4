ca16d15a5769383e9708ce9a1419a083d88215df
xynurm <nyakridha24@gmail.com>
2023-02-03 10:19:20 +0700

    database migration and seeder with faker

---
+++ app/Database/Migrations/2023-02-03-023141_Customer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Customer extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'alamat' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp'
+            
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('customers');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('customers');
+    }
+}
